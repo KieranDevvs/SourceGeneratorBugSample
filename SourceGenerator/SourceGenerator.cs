a79921612155a3bc02de534203fb747568d58042
Kieran Devlin <kierandevvs@gmail.com>
2021-08-22 23:30:40 +0100

    Initial commit.

---
+++ SourceGenerator/SourceGenerator.cs
@@ -0,0 +1,20 @@
+﻿namespace SourceGenerator
+{
+    using System;
+    using Microsoft.CodeAnalysis;
+
+    [Generator]
+    public class SourceGenerator : ISourceGenerator
+    {
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            Console.WriteLine("Debug this.");
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            
+        }
+    }
+}
